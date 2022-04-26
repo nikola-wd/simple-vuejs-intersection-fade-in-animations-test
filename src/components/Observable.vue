7a41be47b1f08723959d635ac429048ffaa9150d
nikola-wd <nikola_wd@hotmail.com>
2022-04-26 13:03:08 +0200

    1 file modified, 1 added

---
+++ src/components/Observable.vue
@@ -0,0 +1,29 @@
+<template>
+  <slot />
+  <i ref="helperRef" />
+</template>
+
+<script>
+import { ref, onMounted } from 'vue';
+export default {
+  props: {
+    mode?: String,
+  },
+  setup(props) {
+    const helperRef = ref(null);
+
+    onMounted(() => {
+      console.log(helperRef); // 0
+
+      if (props.mode) {
+        console.log(props.mode)
+      }
+    }),
+
+    // expose to template and other options API hooks
+    return {
+      helperRef,
+    };
+  }
+}
+</script>
